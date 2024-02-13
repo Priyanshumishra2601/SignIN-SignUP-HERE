104ed64f8b7852def58502aa94a51b03bf793fb7
Priyanshu mishra <116107882+Priyanshumishra2601@users.noreply.github.com>
2024-02-13 16:05:33 +0530

    Add files via upload

---
+++ src/components/SIgn_img.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import img1 from '../components/i1.png'
+import img2 from '../components/i3.png'
+const SIgn_img = () => {
+    return (
+        <>
+            <div className="right_data mt-5" style={{ width: "100%" }}>
+                <div className="sign_img mt-5">
+                    <img src={img1} style={{ maxWidth: 400, maxHeight: 300 }} alt="" />
+                    <img src={img2} style={{ maxWidth: 400, maxHeight: 300 }} alt="" />
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default SIgn_img
