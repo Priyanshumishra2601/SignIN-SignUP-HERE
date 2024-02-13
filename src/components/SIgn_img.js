28e7c3296deb1df52a5c5e4cdee2a7cfb216d8b0
Priyanshu mishra <116107882+Priyanshumishra2601@users.noreply.github.com>
2024-02-13 18:13:38 +0530

    Update SIgn_img.js

--- src/components/SIgn_img.js
+++ src/components/SIgn_img.js
@@ -4,8 +4,9 @@ import img2 from '../components/i3.png'
 const SIgn_img = () => {
     return (
         <>
+        <img src={img1} style={{ maxWidth: 300, maxHeight: 300 }} alt="" />
             <div className="right_data mt-5" style={{ width: "100%" }}>
-    <img src={img1} style={{ maxWidth: 300, maxHeight: 300 }} alt="" />
+    
                 <div className="sign_img mt-5">
                     <img src={img2} style={{ maxWidth: 300, maxHeight: 300 }} alt="" />
                 </div>
